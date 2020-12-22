51a4285284dffa0ef06c156d43a406e094e2bea0
Ahmed Al Salih <ahmed82@users.noreply.github.com>
2020-12-22 11:27:29 -0500

    remove the template implementations

--- class-01/app.js
+++ class-01/app.js
@@ -1,27 +1,6 @@
 new Vue({
   el: '#app',
-  template: `
-  <div>
-    <div class="shape" v-bind:style="shapeStyles" v-on:click="changeShape"></div>
-    <h1>Employees</h1>
-    <table border="1">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Title</th>
-          <th>Company</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(employee, loopIndex) in employees" v-bind:class="{ highlighted: loopIndex % 2 == 0 }">
-          <td>{{ employee.name }}</td>
-          <td>{{ employee.title }}</td>
-          <td>{{ companyName }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  `,
+
   data() { 
     return {
       shapeStyles: {},
